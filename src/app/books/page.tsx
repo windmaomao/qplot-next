313d46f83d892cc20ad83b085d67abd24d2de9ea
Fang Jin <windmaomao@gmail.com>
2024-02-18 14:36:36 -0500

    refine page and book

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -6,7 +6,9 @@ export default function Books() {
     <main className='flex min-h-screen flex-col items-center justify-between p-5'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          <code className='font-mono font-bold'>Fang Jin's Books</code>
+          <Link href='/'>
+            <code className='font-mono font-bold'>Books</code>
+          </Link>
         </p>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
           <a
@@ -40,7 +42,6 @@ export default function Books() {
             <Image
               src='/reacthook.jpg'
               alt='Designing React Hooks The Right Way'
-              className='dark:invert'
               width={200}
               height={400}
               priority
@@ -52,19 +53,6 @@ export default function Books() {
             adopting states using React Hooks
           </p>
         </a>
-
-        <Link
-          href='/'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              &lt;-
-            </span>
-            Back{" "}
-          </h2>
-        </Link>
       </div>
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
         <p className='fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30'>
-          <code className='font-mono font-bold'>Fang Jin</code>
+          <code className='font-mono font-bold'>Home</code>
         </p>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
           <a
@@ -27,8 +27,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"></div>
-
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <div className='flex flex-col gap-5 items-center'>
+          <div className='text-4xl font-bold'>Fang Jin</div>
+          <div className='text-xl font-semibold'>Frontend Engineer</div>
+        </div>
+      </div>
       <div className='mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left'>
         <Link
           href='/books'
@@ -42,7 +46,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            My published books on frontend or programming in general
+            Published books on programming
           </p>
         </Link>
 
@@ -59,7 +63,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Personal blogs on tutorials, tips and philosophy
+            Blogs on programming in general
           </p>
         </a>
 
